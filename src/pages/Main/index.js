f24805b3525b94d9079ccdc82c9fd633722a35fa
Felipe Mariano <86112326+felipemariano29@users.noreply.github.com>
2024-05-02 00:15:40 -0300

    Requesting repos from api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,44 @@
-import React from "react";
-import { FaGithub, FaPlus } from "react-icons/fa";
+import React, { useState, useCallback } from "react";
+import { FaGithub, FaPlus, FaSpinner } from "react-icons/fa";
 import { Container, Form, SubmitButton } from "../../styles";
+import api from "../../services/api";
 
 const Main = () => {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositories, setRepositories] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      async function submit() {
+        setLoading(true);
+
+        try {
+          const response = await api.get(`repos/${newRepo}`);
+
+          const data = {
+            name: response.data.full_name,
+          };
+
+          setRepositories([...repositories, data]);
+          setNewRepo("");
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
+      }
+
+      submit();
+    },
+    [newRepo, repositories]
+  );
+
+  function handleInputChange(e) {
+    setNewRepo(e.target.value);
+  }
+
   return (
     <Container>
       <h1>
@@ -10,11 +46,20 @@ const Main = () => {
         Meus Repositórios
       </h1>
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar repositório" />
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar repositório"
+          value={newRepo}
+          onChange={handleInputChange}
+        />
 
-        <SubmitButton>
-          <FaPlus color="FFF" size={14} />
+        <SubmitButton loading={loading ? 1 : 0}>
+          {loading ? (
+            <FaSpinner color="#FFF" size={14} />
+          ) : (
+            <FaPlus color="#FFF" size={14} />
+          )}
         </SubmitButton>
       </Form>
     </Container>
